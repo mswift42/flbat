3b0348274ae94547387a4a58b433ea90ebd08557
mswift42 <martin.haesler@gmail.com>
2021-12-31 11:52:14 +0000

    implement initState method.

--- lib/batwidget.dart
+++ lib/batwidget.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter/material.dart';
 
 class BatWidget extends StatelessWidget {
@@ -23,8 +26,32 @@ class BwState extends StatefulWidget {
 }
 
 class _BwStateState extends State<BwState> {
+  final Battery _battery = Battery();
+
+  BatteryState? _batteryState;
+  StreamSubscription<BatteryState>? _batteryStateSubscription;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Battery Widget'),
+      ),
+      body: Center(
+        child: Column(
+          children: [const Text('Battery ')],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _batteryStateSubscription =
+        _battery.onBatteryStateChanged.listen((BatteryState state) {
+      setState(() {
+        _batteryState = state;
+      });
+    });
   }
 }
